5868fff38fbec5df45386bea918a1274af0d0223
AcesofBases <83755522+AcesofBases@users.noreply.github.com>
2021-08-07 21:02:09 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var console1="My First console project";
+console.log(console1);
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add);
+var first_name="Ace";
+var last_name="Dineen";
+var full_name=first_name.concat(last_name);
+console.log(full_name);   
+
+var s1=Math.sqrt(25);
+console.log(s1); 
+var max=Math.max(1,34,50,25,49);
+console.log(max);
+var min=Math.min(219,2,482,674,10);
+console.log(min);
